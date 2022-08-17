47e9946a588f13268b9eb1c20b3734424993e2c1
Umang0001 <umangbheda001@gmail.com>
2022-08-17 21:17:38 +0530

    17/08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { FeaturesComponent } from './features/features.component';
+import { Banner2Component } from './banner2/banner2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    FeaturesComponent,
+    Banner2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule
